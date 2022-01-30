9ef938c24df0107fef6f5e46ace48d2b4b89b7f8
estebanrucan <errucan@uc.cl>
2022-01-30 12:09:16 -0300

    V1.1 finished

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Navbar 
     const toggle_btn = document.querySelector(".toggle-btn");
     const navbar = document.querySelector(".navbar");
 
     toggle_btn.addEventListener("click", () => {
         navbar.classList.toggle("mobile-nav")
     })
+
+    // Animations
+
+    AOS.init({
+        offset: 200,
+        delay: 100,
+        duration: 1000,
+      });
 
 })
