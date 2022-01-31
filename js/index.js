945f6ede36a45b1a1a1a777d8e59b7eb2b28b7a1
estebanrucan <errucan@uc.cl>
2022-01-31 19:05:46 -0300

    V1.2 Modo oscuro & scroll to top button

--- js/index.js
+++ js/index.js
@@ -6,4 +6,28 @@ document.addEventListener("DOMContentLoaded", () => {
     toggle_btn.addEventListener("click", () => {
         navbar.classList.toggle("mobile-nav")
     })
+
+    // Scroll Btn
+    const scrollBtn = document.getElementById("scrolltop");
+    scrollBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    });
+    window.addEventListener("scroll", () => {
+        if (window.scrollY >= 900) {
+            scrollBtn.style.opacity = 1;
+        } else {
+            scrollBtn.style.opacity = 0;
+        }
+    });
+
+    // Theme toggle
+    const themeToggle = document.getElementById("checkbox");
+    const body = document.querySelector("body");
+    themeToggle.addEventListener("change", () => {
+        body.classList.toggle("dark");
+    })
 })
